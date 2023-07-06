768d058b8f823373c31fec7cf9406b6bfbb16a2f
leulkashay <leulkashay2012@gmail.com>
2023-07-06 18:09:11 +0000

    task done

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+a typed-annotation function
+"""
+from typing import List
+
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    sum list of float
+    """
+    sum = 0
+    for item in input_list:
+        sum += item
+    return sum
