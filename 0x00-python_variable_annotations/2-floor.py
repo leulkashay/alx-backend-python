ada42d1c9adf08d90b5a985f5acad7b0ccd88611
leulkashay <leulkashay2012@gmail.com>
2023-07-06 17:56:08 +0000

    task done

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function annotation
+"""
+
+
+def floor(n: float) -> int:
+    """
+    a typed-annotated func floor
+    that floor the given number
+    """
+    return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+function annotation
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    a typed-annotated func to_str
+    that takes a float n and returns
+    the string representation of it
+    """
+    return str(n)
