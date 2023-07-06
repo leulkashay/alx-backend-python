39ed93850ea06e8cf1a1ec4e069148f1641bf0e5
leulkashay <leulkashay2012@gmail.com>
2023-07-06 17:43:44 +0000

    first task

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function annotation
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    a typed-annotated func concat
+    that concatenates two string
+    """
+    return str1 + str2
