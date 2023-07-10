145656e41c638f63066b529625c569eb6312ac0e
leulkashay <leulkashay2012@gmail.com>
2023-07-10 18:54:50 +0000

    first

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python3
 """
-multiple coroutines with async
+synchronous function
 """
-from typing import List
 import asyncio
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
-task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    execute multiple coroutines at the same time with async
+    a function `task_wait_random` that
+    takes an integer `max_delay` and
+    returns a asyncio.Task
     """
-    result = await asyncio.gather(*(task_wait_random(max_delay)
-                                    for _ in range(n)))
+    Task = asyncio.create_task(wait_random(max_delay))
 
-    for i in range(len(result)):
-        for j in range(i+1, len(result)):
-            if (result[i] > result[j]):
-                result[i], result[j] = result[j], result[i]
-
-    return result
+    return Task
