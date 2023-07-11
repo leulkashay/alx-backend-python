8fc65e4f5b98642428e6d5c07e167910b7d42520
leulkashay <leulkashay2012@gmail.com>
2023-07-11 23:21:58 +0000

    first commit

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Asynchronous comerhension
+"""
+import asyncio
+import random
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    The coroutine will loop 10 times,
+    each time async wait 1 sec, then
+    yield a random number between 0 and 10
+    """
+    for i range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
